f1ac6fbe50a9155c7524d442dbf5ab07fc723220
Sitaram Nikhil Mungamuri <sitaramnikhil@usf.edu>
2024-02-27 15:08:36 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace New_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
